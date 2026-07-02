e5530168b08754272fd1cc2a202afc08ef5164ef
rltjdms <137126199+rltjdms@users.noreply.github.com>
2026-07-02 19:14:34 +0900

    [Fix} Main_Youtube link

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -126,7 +126,7 @@ function Home (props) {
                                <iframe
                                 width="560"
                                 height="315"
-                                src="https://www.youtube.com/embed/VIDEO_ID?rel=0"
+                               src="https://www.youtube.com/embed/dQw4w9WgXcQ"
                                 title="YouTube video player"
                                 frameBorder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
