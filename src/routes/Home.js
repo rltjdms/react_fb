218e5d7d8fee8c36ada819c004b4a88ce8c8be72
rltjdms <137126199+rltjdms@users.noreply.github.com>
2026-07-02 17:40:51 +0900

    [Fix} Main_Youtube link

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -123,7 +123,7 @@ function Home (props) {
                     <Row className="align-items-center">
                         <div className="video col">
                             <div className="video_area">
-                                <iframe width="560" height="315" src="https://youtu.be/rhncD7HRbcQ?si=5nWi2k1xGznAUNh3" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen=""></iframe>
+                               <iframe width="560" height="315" src="https://www.youtube.com/embed/rhncD7HRbcQ?si=5nWi2k1xGznAUNh3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                             </div>
                         </div>
                         <SwiperBox className="col">
